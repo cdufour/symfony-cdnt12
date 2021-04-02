6e73851ab0e0666bcbceea81a0e5399ac300ea57
cdufour <opusidea@gmail.com>
2021-04-02 15:32:11 +0000

    j2 end

--- src/Controller/ExoController.php
+++ src/Controller/ExoController.php
@@ -2,10 +2,15 @@
 
 namespace App\Controller;
 
+use App\Entity\City;
+use App\Repository\CityRepository;
+use App\Repository\CountryRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use App\Service\CalculatorService;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\HttpFoundation\Request;
 
 class ExoController extends AbstractController
 {
@@ -25,5 +30,50 @@ class ExoController extends AbstractController
         $square = $calculator->square($num);
 
         return new Response($square);
+    }
+
+    /**
+     * @Route("/exos/city/form", name="city_form")
+     */
+    public function form(
+        Request $req, 
+        EntityManagerInterface $em,
+        CountryRepository $countryRepo
+        ): Response
+    {
+        if ($req->getMethod() === Request::METHOD_POST) {
+
+            $name = $req->request->get('name');
+            $mayor = $req->request->get('mayor');
+            $countryId = $req->request->get('country');
+            $country = $countryRepo->find($countryId);
+
+            $city = new City();
+            $city
+                ->setName($name)
+                ->setMayor($mayor)
+                ->setCountry($country);
+            
+            $em->persist($city);
+            $em->flush($city);
+
+            return $this->redirectToRoute('city_list');
+        }
+
+        $countries = $countryRepo->findBy([], ['name' => 'ASC']);
+
+        return $this->render('city/form.html.twig', [
+            'countries' => $countries
+        ]);
+    }
+
+    /**
+     * @Route("/exos/city/list", name="city_list")
+     */
+    public function list(CityRepository $cityRepo)
+    {
+        return $this->render('city/list.html.twig', [
+            'cities' => $cityRepo->findAll()
+        ]);
     }
 }
---
+++ src/Entity/City.php
@@ -0,0 +1,75 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\CityRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass=CityRepository::class)
+ */
+class City
+{
+    /**
+     * @ORM\Id
+     * @ORM\GeneratedValue
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $name;
+
+    /**
+     * @ORM\Column(type="string", length=100)
+     */
+    private $mayor;
+
+    /**
+     * @ORM\ManyToOne(targetEntity=Country::class)
+     */
+    private $country;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getName(): ?string
+    {
+        return $this->name;
+    }
+
+    public function setName(string $name): self
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    public function getMayor(): ?string
+    {
+        return $this->mayor;
+    }
+
+    public function setMayor(string $mayor): self
+    {
+        $this->mayor = $mayor;
+
+        return $this;
+    }
+
+    public function getCountry(): ?Country
+    {
+        return $this->country;
+    }
+
+    public function setCountry(?Country $country): self
+    {
+        $this->country = $country;
+
+        return $this;
+    }
+}
